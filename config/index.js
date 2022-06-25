8b6208c14d9942a1e11f80e1e49e89a06d60b2ae
lordSosa15 <97013669+lordSosa15@users.noreply.github.com>
2022-06-25 10:08:54 -0400

    cors fix

--- config/index.js
+++ config/index.js
@@ -19,25 +19,25 @@ module.exports = (app) => {
   // Because this is a server that will accept requests from outside and it will be hosted ona server with a `proxy`, express needs to know that it should trust that setting.
   // Services like heroku use something called a proxy and you need to add this to your server
   app.set("trust proxy", 1);
-  
+
   // controls a very specific header to pass headers from the frontend
   app.use(
     cors({
       credentials: true,
-      origin: '*' || process.env.ORIGIN || "http://localhost:3000",
+      origin: process.env.ORIGIN || "http://localhost:3000",
     })
   );
 
   // In development environment the app logs
   app.use(logger("dev"));
 
-  app.post('/', async(req, res)=>{
-    console.log(req.body)
-    res.json("I have received your data")
-  })
+  app.post("/", async (req, res) => {
+    console.log(req.body);
+    res.json("I have received your data");
+  });
 
   // To have access to `body` property in the request
-  app.use(express.json({limit: '50mb'}));
-  app.use(express.urlencoded({ extended: false, limit: '50mb' }));
+  app.use(express.json({ limit: "50mb" }));
+  app.use(express.urlencoded({ extended: false, limit: "50mb" }));
   app.use(cookieParser());
 };
--- server.js
+++ server.js
@@ -5,6 +5,10 @@ const express = require('express');
 // const app = express();
 app.use(express.static('public'));
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  next();
+});
 
 // ℹ️ Sets the PORT for our app to have access to it. If no env has been set, we hard code it to 3000
 const PORT = process.env.PORT || 5005;
